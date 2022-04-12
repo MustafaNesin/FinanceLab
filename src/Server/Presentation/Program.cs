c6ac6bc0dee847940d09e1907d453ffff4983e0d
Mustafa Nesin <mstf.nesin@gmail.com>
2022-04-12 18:22:09 +0300

    Server configurations

--- src/Server/Presentation/Program.cs
+++ src/Server/Presentation/Program.cs
@@ -1,17 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
