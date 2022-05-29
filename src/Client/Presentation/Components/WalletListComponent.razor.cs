5a74f0b552ae5e110c80ba58462ef1184222d7e1
baariskose <61475377+baariskose@users.noreply.github.com>
2022-05-29 23:54:01 +0300

    Show Wallet List  depends on Admin or User

---
+++ src/Client/Presentation/Components/WalletListComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using FinanceLab.Shared.Application.Constants;
+using FinanceLab.Shared.Domain.Models.Dtos;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace FinanceLab.Client.Presentation.Components;
+
+public partial class WalletListComponent
+{
+    [Parameter]
+    public string? UserName { get; set; }
+
+    private Task<TableData<WalletDto>> GetTableDataAsync(TableState tableState)
+        => base.GetTableDataAsync(tableState, ApiRouteConstants.GetWalletList, UserName);
+}
---
+++ src/Client/Presentation/Pages/WalletListPage.razor.cs
@@ -0,0 +1,20 @@
+﻿using FinanceLab.Shared.Application.Constants;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components;
+
+namespace FinanceLab.Client.Presentation.Pages;
+
+[UsedImplicitly]
+public partial class WalletListPage
+{
+    [Parameter]
+    public string? UserName { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        if (UserName is not null &&
+            UserName != StateContainer.User?.UserName &&
+            StateContainer.User?.Role is not RoleConstants.Admin)
+            NavigationManager.NavigateTo("/Wallets");
+    }
+}
