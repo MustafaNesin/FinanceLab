ecbf86b153a325e51a58b72225712fec19bdace1
baariskose <61475377+baariskose@users.noreply.github.com>
2022-05-29 23:28:48 +0300

    Create main layout

---
+++ src/Client/Presentation/Components/NavigationMenuComponent.razor.cs
@@ -0,0 +1,44 @@
+﻿using MudBlazor;
+
+namespace FinanceLab.Client.Presentation.Components;
+
+public partial class NavigationMenuComponent
+{
+    private readonly MudTheme MuudstrapTheme = new()
+    {
+        Palette = new Palette
+        {
+            AppbarBackground = "#000000",
+            AppbarText = "#000000"
+        },
+        Typography = new Typography
+        {
+            H6 = new H6
+            {
+                FontFamily = new[]
+                {
+                    "system-ui", "-apple-system", "Segoe UI", "Roboto ", "Helvetica Neue", "Arial", "Noto Sans",
+                    "Liberation Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+                    "Noto Color Emoji"
+                },
+                FontSize = "1.25rem",
+                FontWeight = 400,
+                LineHeight = 1.7,
+                LetterSpacing = "normal"
+            },
+            Button = new Button
+            {
+                FontFamily = new[]
+                {
+                    "system-ui", "-apple-system", "Segoe UI", "Roboto ", "Helvetica Neue", "Arial", "Noto Sans",
+                    "Liberation Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
+                    "Noto Color Emoji"
+                },
+                FontSize = "1rem",
+                FontWeight = 400,
+                LineHeight = 1.5,
+                LetterSpacing = "normal"
+            }
+        }
+    };
+}
