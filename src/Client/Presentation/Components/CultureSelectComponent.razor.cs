c92c1bb20cd444d2ebcf40c9564fe39b3c90d2aa
ilaydakaraduman <61521695+ilaydakaraduman@users.noreply.github.com>
2022-05-31 16:12:59 +0300

    navbar guncellemeler

--- src/Client/Presentation/Components/CultureSelectComponent.razor.cs
+++ src/Client/Presentation/Components/CultureSelectComponent.razor.cs
@@ -8,6 +8,16 @@ public partial class CultureSelectComponent
     [Inject]
     private ISyncLocalStorageService LocalStorage { get; set; } = default!;
 
+    public string currLang { get; set; } = default!;
+    public string currLangTx { get; set; } = default!;
+    protected override void OnInitialized()
+    {
+        currLang = LocalStorage.GetItemAsString("Culture");
+        if (currLang == "en-US")
+            currLangTx = "English";
+        else if (currLang == "tr-TR")
+            currLangTx = "Türkçe";
+    }
     private void ChangeLang(string value)
     {
         LocalStorage.SetItemAsString("Culture", value);
--- src/Client/Presentation/Components/NavigationMenuComponent.razor.cs
+++ src/Client/Presentation/Components/NavigationMenuComponent.razor.cs
@@ -5,6 +5,7 @@ public partial class NavigationMenuComponent
     private string? _fullName;
     private string? _userName;
     private string? _userUri;
+    private string? _avatar;
 
     protected override async Task OnParametersSetAsync()
     {
@@ -15,9 +16,12 @@ public partial class NavigationMenuComponent
 
         var firstName = StateContainer.User.FirstName;
         var lastName = StateContainer.User.LastName;
+        var avatar =StateContainer.User.FirstName.Substring(0,1)+StateContainer.User.LastName.Substring(0,1)+" "; 
 
         _userName = StateContainer.User.UserName;
         _userUri = $"/Users/{_userName}";
         _fullName = $"{firstName} {lastName}";
+        _avatar = $"{avatar}";
     }
+    
 }
