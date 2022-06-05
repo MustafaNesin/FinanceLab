2614017274297b6721c22e9c53929257bdb08dd1
Mustafa Nesin <mstf.nesin@gmail.com>
2022-06-05 17:23:31 +0300

    Update navbar changes

--- src/Client/Presentation/Components/CultureSelectComponent.razor.cs
+++ src/Client/Presentation/Components/CultureSelectComponent.razor.cs
@@ -5,20 +5,17 @@ namespace FinanceLab.Client.Presentation.Components;
 
 public partial class CultureSelectComponent
 {
+    private string _cultureLabel = default!;
+
     [Inject]
     private ISyncLocalStorageService LocalStorage { get; set; } = default!;
 
-    public string currLang { get; set; } = default!;
-    public string currLangTx { get; set; } = default!;
     protected override void OnInitialized()
     {
-        currLang = LocalStorage.GetItemAsString("Culture");
-        if (currLang == "en-US")
-            currLangTx = "English";
-        else if (currLang == "tr-TR")
-            currLangTx = "Türkçe";
+        _cultureLabel = LocalStorage.GetItemAsString("Culture") == "tr-TR" ? "Türkçe" : "English";
     }
-    private void ChangeLang(string value)
+
+    private void ChangeCulture(string value)
     {
         LocalStorage.SetItemAsString("Culture", value);
         NavigationManager.NavigateTo(NavigationManager.Uri, true);
